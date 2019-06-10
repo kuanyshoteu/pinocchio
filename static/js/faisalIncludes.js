518ae81454188f940952940120d047fed623b081
Faisal Iraqi <faisal.iraqi99@gmail.com>
2019-06-10 20:17:45 +0600

    number-counter ready

--- static/js/faisalIncludes.js
+++ static/js/faisalIncludes.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
   let filterItem = $('.filter-item');
 
   $(document.body).on('click', function(e){
-    if($(e.target).closest('.filter-element').length === 0) {
+    if($(e.target).closest(filterElement).length === 0) {
       filterList.addClass('filter-list-hide');
     }
   });
@@ -22,4 +22,40 @@ $(document).ready(function(){
   });
 
 // Filter-list end
+// Number counter start
+
+  let number = document.getElementById('number-counter');
+
+  console.log();
+
+  let start = 20;
+  let end = +number.textContent;
+  let ticks = 20;
+  let speed = 50;
+
+  let randomNumbers = [end]
+
+  for (let i = 0; i < ticks - 1; i++) {
+    randomNumbers.unshift(
+      Math.floor(Math.random() * (end - start + 1) + start)
+    );
+  }
+
+  randomNumbers.sort((a, b) => { return a - b });
+
+  console.log(randomNumbers.length)
+
+  let x = 0;
+  let interval = setInterval(function () {
+
+    number.innerHTML = randomNumbers.shift();
+
+    if (++x === ticks) {
+      window.clearInterval(interval);
+    }
+
+  }, speed);
+
+// Number counter end
+
 });
