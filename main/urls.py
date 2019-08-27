a11404b8b133240082f74e3eb1a35d1c4e86b870
Kuanysh Oteu <kuanyshoteu@gmail.com>
2019-08-28 01:55:53 +0600

    front bug fixes

--- main/urls.py
+++ main/urls.py
@@ -30,5 +30,6 @@ urlpatterns = [
     url(r'^api/make_zaiavka/$', make_zaiavka, name='make_zaiavka'),
     url(r'^api/get_request_land/$', get_request_land, name='get_request_land'),
     url(r'^robots.txt', robots, name="robots"),
+    url(r'^Robots.txt', robots, name="robots"),
     url(r'^about/robots.txt', robots, name="robots"),    
 ]
