3a87ab4b05e3c88999f453ca84b9ae7771d473ed
Kuanysh Oteu <kuanyshoteu@gmail.com>
2019-08-28 00:09:59 +0600

    front bug fixes

--- tripleA/settings.py
+++ tripleA/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
-toserver = False
+toserver = True
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'c2+b9fu@bhw=2r-$+ge)7p*l2vx^r%nsb!ivy9le=laznpug%0'
 
