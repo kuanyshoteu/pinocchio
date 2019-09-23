748f583ad034382716ceb6644ae8dd32670b7d19
Kuanysh Oteu <kuanyshoteu@gmail.com>
2019-09-23 15:52:41 +0600

    clock scheduler

--- clock.py
+++ clock.py
@@ -1,11 +1,13 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
-
+from subjects.models import Subject
 sched = BlockingScheduler()
 
 @sched.scheduled_job('interval', minutes=1)
 def timed_job():
     print('************************************')
     print('This job is run every three minutes.')
+    print(Subject.objects.first().title)
+
 
 @sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
 def scheduled_job():
