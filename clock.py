496baf1de4d2a336ca87c0231a6c7234e92e5e92
Kuanysh Oteu <kuanyshoteu@gmail.com>
2019-09-24 03:06:44 +0600

    clock scheduler

--- clock.py
+++ clock.py
@@ -1,9 +1,12 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 import requests
+from pytz import utc
 
 sched = BlockingScheduler()
+sched.add_job(scheduled_job,'cron',
+   day_of_week='mon-sat', hour='03-13',
+   timezone='Asia/Almaty')
 
-@sched.scheduled_job('cron', day_of_week='mon-sat', hour=03, timezone='Asia/Almaty')
 def scheduled_job():
     print('******************')
     url = 'https://www.bilimtap.kz/subjects/api/update_cards_money?secret=NJf5wefewfm58keijnw'
