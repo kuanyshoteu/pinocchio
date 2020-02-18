00fcd4af714fcecc3c0c26d0f3df59e80bcf7dcc
Kuanysh Oteu <kuanyshoteu@gmail.com>
2020-02-18 11:58:47 +0600

    office + mobile menu

--- tripleA/settings.py
+++ tripleA/settings.py
@@ -18,13 +18,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
-toserver = True
+toserver = False
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'c2+b9fu@bhw=2r-$+ge)7p*l2vx^r%nsb!ivy9le=laznpug%0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 if toserver:
-    DEBUG = True
+    DEBUG = False
     ALLOWED_HOSTS = ['www.bilimtap.kz', 'bilimtap.kz', 'testbilimtap.herokuapp.com']
 else:
     DEBUG = True
