273122fe72730c6c3809b18f06a4b28b0517d08f
Kuanysh Oteu <kuanyshoteu@gmail.com>
2020-03-25 07:10:46 +0600

    f

--- schools/views.py
+++ schools/views.py
@@ -351,16 +351,16 @@ def vk_connecting(request):
         vk.user_id = user_id
         vk.access_token = access_token
 
-        get_groups_url = 'https://api.vk.com/method/account.getProfileInfo'
-        data = {
-            'access_token':access_token,
-            'v':'5.103',        
-        }
-        r = requests.post(get_groups_url,data=data,allow_redirects=True)
-        a = json.loads(r.content)
-        print(a)
-        a = a['response']
-        vk.username = a['first_name'] + a['last_name']
+        # getProfileInfo_url = 'https://api.vk.com/method/account.getProfileInfo'
+        # data = {
+        #     'access_token':access_token,
+        #     'v':'5.103',        
+        # }
+        # r = requests.post(getProfileInfo_url,data=data,allow_redirects=True)
+        # a = json.loads(r.content)
+        # print(a)
+        # a = a['response']
+        # vk.username = a['first_name'] + a['last_name']
 
         get_groups_url = 'https://api.vk.com/method/groups.get'
         data = {
