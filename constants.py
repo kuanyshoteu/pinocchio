b2530be6548a08c5521ca662544fdf7fc26d5c5e
Kuanysh Oteu <kuanyshoteu@gmail.com>
2020-08-21 06:13:40 +0600

    cloudpayments

--- constants.py
+++ constants.py
@@ -16,7 +16,7 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 import json
 
 cloudpayments_id = 'pk_8f05633d39130c02981943d45a4a4'
-cloudpayments_secretkey = 'be5fb1c28aff24ce84ef4cbc090d4152'
+cloudpayments_secretkey = '933e77aa9d8497d3149d550f6e1e4873'
 
 def send_sms(phones, message, time):
     login = 'Pinocchio'
